10a10216810d58dc5657734a57cd6d0da59d58ba
nadro-j <n4dro@protonmail.com>
2024-05-03 02:36:06 +0100

    --call_data option added to get the encoded call of a proposal

--- chainstate.py
+++ chainstate.py
@@ -13,7 +13,7 @@ class MaterializedChainState:
             print(f"Unable to connect: {error.args}")
             exit()
 
-    def ref_caller(self, index: int):
+    def ref_caller(self, index: int, call_data: bool):
         try:
             referendum = self.substrate.query(module='Referenda', storage_function='ReferendumInfoFor', params=[index]).serialize()
             if referendum is None or 'Ongoing' not in referendum:
@@ -23,18 +23,24 @@ class MaterializedChainState:
 
             if 'Inline' in preimage:
                 call = preimage['Inline']
-                call_obj = self.substrate.create_scale_object('Call')
-                decoded_call = call_obj.decode(ScaleBytes(call))
-                return json.dumps(decoded_call, indent=4)
+                if not call_data:
+                    call_obj = self.substrate.create_scale_object('Call')
+                    decoded_call = call_obj.decode(ScaleBytes(call))
+                    return json.dumps(decoded_call, indent=4)
+                else:
+                    return call
 
             if 'Lookup' in preimage:
                 preimage_hash = preimage['Lookup']['hash']
                 preimage_length = preimage['Lookup']['len']
 
                 call = self.substrate.query(module='Preimage', storage_function='PreimageFor', params=[(preimage_hash, preimage_length)]).value
-                call_obj = self.substrate.create_scale_object('Call')
-                decoded_call = call_obj.decode(ScaleBytes(call))
-                return json.dumps(decoded_call, indent=4)
+                if not call_data:
+                    call_obj = self.substrate.create_scale_object('Call')
+                    decoded_call = call_obj.decode(ScaleBytes(call))
+                    return json.dumps(decoded_call, indent=4)
+                else:
+                    return call
         except Exception as ref_caller_error:
             raise ref_caller_error
 
@@ -43,11 +49,12 @@ def main():
     parser = argparse.ArgumentParser(description='Interact with the Polkadot/Substrate blockchain via SubstrateInterface')
     parser.add_argument('--url', default="wss://rpc.ibp.network/polkadot", help='Specify the URL of the blockchain (default: wss://rpc.ibp.network/polkadot)')
     parser.add_argument('--ref_caller', type=int, help='Index of the referendum')
+    parser.add_argument('--call_data', action='store_true', default=False, help='Flag to indicate whether --call_data is specified')
     args = parser.parse_args()
     chain_state = MaterializedChainState(url=args.url)
 
     if args.ref_caller:
-        result = chain_state.ref_caller(args.ref_caller)
+        result = chain_state.ref_caller(args.ref_caller, args.call_data)
         print(result)
 
 
